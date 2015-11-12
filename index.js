a43c002cb8a650349d2f8d55c809bf54e7c0e469
Mark Stier <stier020@Marks-MacBook-Pro.local>
2015-11-12 11:42:59 -0600

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict"
+
+var Worker = require("basic-distributed-computation").Worker;
+var camera = require('camera-usb');
+
+class Capture extends Worker {
+  constructor(parent){
+    super("capture-image", parent);
+  }
+
+  work(req, inputKey, outputKey){
+    if(err){
+      req.status(err).next();
+    } else {
+      if(outputKey){
+        req.body[outputKey] = camera.capture();
+      } else {
+        req.body = camera.capture();
+      }
+      req.next();
+    }
+  }
+}
+
+module.exports = Capture;
